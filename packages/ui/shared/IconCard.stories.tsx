d50afdfe7f6cfcfd3cafba03da84038d7a24aca2
Tomomi Kimura <tomotomy73@gmail.com>
2022-11-01 19:48:16 +0900

    feat: Add light and dark themes

--- packages/ui/shared/IconCard.stories.tsx
+++ packages/ui/shared/IconCard.stories.tsx
@@ -5,30 +5,32 @@ export default {
     component: IconCard,
 } as ComponentMeta<typeof IconCard>;
 
-export const WithDescriptionPrimary: ComponentStoryObj<typeof IconCard> = {
+export const WithDescriptionLight: ComponentStoryObj<typeof IconCard> = {
     args: {
+        theme: "light",
         title: "DISCOVER",
         description:
             "Visit the learning center to access buyer’s guides, project checklists, and pricing guides.",
     },
 };
-
-//FIXME: Can I use different module.css from the primary? e.x. Different background and font color
-export const WithDescriptionSecondary: ComponentStoryObj<typeof IconCard> = {
+export const WithDescriptionDark: ComponentStoryObj<typeof IconCard> = {
     args: {
+        theme: "dark",
         title: "PROJECT COORDINATION FROM START TO FINISH",
         description:
             "A dedicated project manager is here to serve from inspection to de-installation, crating, rigging, transport, importation support, and installation.",
     },
 };
 
-export const WithoutDescriptionPrimary: ComponentStoryObj<typeof IconCard> = {
+export const WithoutDescriptionLight: ComponentStoryObj<typeof IconCard> = {
     args: {
+        theme: "light",
         title: "DISCOVER",
     },
 };
-export const WithoutDescriptionSecondary: ComponentStoryObj<typeof IconCard> = {
+export const WithoutDescriptionDark: ComponentStoryObj<typeof IconCard> = {
     args: {
+        theme: "dark",
         title: "PROJECT COORDINATION FROM START TO FINISH",
     },
 };
--- packages/ui/shared/IconCard.tsx
+++ packages/ui/shared/IconCard.tsx
@@ -3,30 +3,36 @@ import * as React from "react";
 import styles from "./IconCard.module.css";
 
 export type IconCardProps = {
+    theme: "light" | "dark";
     title: string;
     //FIXME: Add logo
     description: string;
 };
 
-//FIXME: Fix modules.css so that it doesn't use padding-left for description
-export const IconCard: React.FC<IconCardProps> = ({ title, description }) => {
+export const IconCard: React.FC<IconCardProps> = ({
+    theme,
+    title,
+    //FIXME: Add logo
+    description,
+}) => {
+    const themes = styles[theme];
     return (
-        <div className={styles.iconCard}>
-            <div className={styles.iconCardContent}>
+        <div className={`${styles.iconCard} ${themes}`}>
+            <div className={styles.content}>
                 <div className={styles.logo} />
                 <div className={styles.title}>{title}</div>
                 <div className={styles.description}>
                     {description !== undefined ? description : undefined}
                 </div>
             </div>
-            <div className={styles.iconCardContent}>
+            <div className={styles.content}>
                 <div className={styles.logo} />
                 <div className={styles.title}>{title}</div>
                 <div className={styles.description}>
                     {description !== undefined ? description : undefined}
                 </div>
             </div>
-            <div className={styles.iconCardContent}>
+            <div className={styles.content}>
                 <div className={styles.logo} />
                 <div className={styles.title}>{title}</div>
                 <div className={styles.description}>
