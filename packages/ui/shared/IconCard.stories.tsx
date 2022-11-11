abb490836f1d64ec1ccc0f32dee3ba19b1a2c016
Tomomi Kimura <tomotomy73@gmail.com>
2022-11-11 12:06:38 +0900

    chore: format files after config change

--- packages/ui/shared/IconCard.stories.tsx
+++ packages/ui/shared/IconCard.stories.tsx
@@ -2,51 +2,50 @@ import { IconCard } from "./IconCard";
 import { ComponentMeta, ComponentStoryObj } from "@storybook/react";
 
 export default {
-    component: IconCard,
+  component: IconCard,
 } as ComponentMeta<typeof IconCard>;
 
 export const WithDescriptionLight: ComponentStoryObj<typeof IconCard> = {
-    args: {
-        theme: "light",
-        logo: {
-            alt: "Discovery logo",
-            src: "https://via.placeholder.com/50x50/FFFFFF",
-        },
-        title: "DISCOVER",
-        description:
-            "Visit the learning center to access buyer’s guides, project checklists, and pricing guides.",
+  args: {
+    theme: "light",
+    logo: {
+      alt: "Discovery logo",
+      src: "https://via.placeholder.com/50x50/FFFFFF",
     },
+    title: "DISCOVER",
+    description: "Visit the learning center to access buyer’s guides, project checklists, and pricing guides.",
+  },
 };
 export const WithDescriptionDark: ComponentStoryObj<typeof IconCard> = {
-    args: {
-        theme: "dark",
-        logo: {
-            alt: "Discovery logo",
-            src: "https://via.placeholder.com/50x50/000000",
-        },
-        title: "PROJECT COORDINATION FROM START TO FINISH",
-        description:
-            "A dedicated project manager is here to serve from inspection to de-installation, crating, rigging, transport, importation support, and installation.",
+  args: {
+    theme: "dark",
+    logo: {
+      alt: "Discovery logo",
+      src: "https://via.placeholder.com/50x50/000000",
     },
+    title: "PROJECT COORDINATION FROM START TO FINISH",
+    description:
+      "A dedicated project manager is here to serve from inspection to de-installation, crating, rigging, transport, importation support, and installation.",
+  },
 };
 
 export const WithoutDescriptionLight: ComponentStoryObj<typeof IconCard> = {
-    args: {
-        theme: "light",
-        logo: {
-            alt: "Discovery logo",
-            src: "https://via.placeholder.com/50x50/FFFFFF",
-        },
-        title: "DISCOVER",
+  args: {
+    theme: "light",
+    logo: {
+      alt: "Discovery logo",
+      src: "https://via.placeholder.com/50x50/FFFFFF",
     },
+    title: "DISCOVER",
+  },
 };
 export const WithoutDescriptionDark: ComponentStoryObj<typeof IconCard> = {
-    args: {
-        theme: "dark",
-        logo: {
-            alt: "Discovery logo",
-            src: "https://via.placeholder.com/50x50/000000",
-        },
-        title: "PROJECT COORDINATION FROM START TO FINISH",
+  args: {
+    theme: "dark",
+    logo: {
+      alt: "Discovery logo",
+      src: "https://via.placeholder.com/50x50/000000",
     },
+    title: "PROJECT COORDINATION FROM START TO FINISH",
+  },
 };
--- packages/ui/shared/IconCard.tsx
+++ packages/ui/shared/IconCard.tsx
@@ -3,63 +3,34 @@ import * as React from "react";
 import styles from "./IconCard.module.css";
 
 export type IconCardProps = {
-    theme: "light" | "dark";
-    title: string;
-    logo: {
-        alt: string;
-        src: string;
-    };
-    description: string;
+  theme: "light" | "dark";
+  title: string;
+  logo: {
+    alt: string;
+    src: string;
+  };
+  description: string;
 };
 // FIXME: Change theme, title, description of each component in each type
-export const IconCard: React.FC<IconCardProps> = ({
-    theme,
-    title,
-    logo,
-    description,
-}) => {
-    const themes = styles[theme];
-    return (
-        <div className={`${styles.iconCard} ${themes}`}>
-            <div className={styles.content}>
-                <img
-                    className={styles.logo}
-                    width="50"
-                    height="50"
-                    alt={logo.alt}
-                    src={logo.src}
-                />
-                <div className={styles.title}>{title}</div>
-                <div className={styles.description}>
-                    {description !== undefined ? description : undefined}
-                </div>
-            </div>
-            <div className={styles.content}>
-                <img
-                    className={styles.logo}
-                    width="50"
-                    height="50"
-                    alt={logo.alt}
-                    src={logo.src}
-                />
-                <div className={styles.title}>{title}</div>
-                <div className={styles.description}>
-                    {description !== undefined ? description : undefined}
-                </div>
-            </div>
-            <div className={styles.content}>
-                <img
-                    className={styles.logo}
-                    width="50"
-                    height="50"
-                    alt={logo.alt}
-                    src={logo.src}
-                />
-                <div className={styles.title}>{title}</div>
-                <div className={styles.description}>
-                    {description !== undefined ? description : undefined}
-                </div>
-            </div>
-        </div>
-    );
+export const IconCard: React.FC<IconCardProps> = ({ theme, title, logo, description }) => {
+  const themes = styles[theme];
+  return (
+    <div className={`${styles.iconCard} ${themes}`}>
+      <div className={styles.content}>
+        <img className={styles.logo} width="50" height="50" alt={logo.alt} src={logo.src} />
+        <div className={styles.title}>{title}</div>
+        <div className={styles.description}>{description !== undefined ? description : undefined}</div>
+      </div>
+      <div className={styles.content}>
+        <img className={styles.logo} width="50" height="50" alt={logo.alt} src={logo.src} />
+        <div className={styles.title}>{title}</div>
+        <div className={styles.description}>{description !== undefined ? description : undefined}</div>
+      </div>
+      <div className={styles.content}>
+        <img className={styles.logo} width="50" height="50" alt={logo.alt} src={logo.src} />
+        <div className={styles.title}>{title}</div>
+        <div className={styles.description}>{description !== undefined ? description : undefined}</div>
+      </div>
+    </div>
+  );
 };
