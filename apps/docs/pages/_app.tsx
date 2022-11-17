23eb74e377892511bf0857aba66fb94534bc2aad
Tomomi Kimura <tomotomy73@gmail.com>
2022-11-17 14:57:16 +0900

    feat: install Bootstrap icons

--- apps/docs/pages/_app.tsx
+++ apps/docs/pages/_app.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { AppProps } from "next/app";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 // Add here all global stylesheets
 import "ui/shared/variables.css";
 
 export const AdminApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <Component {...pageProps}>
+      <i className="bi bi-0-circle"></i>
+    </Component>
+  );
 };
 
 // Used by Next.js
