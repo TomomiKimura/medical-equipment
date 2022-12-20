9c291117bea8a5539374002e88d52952faeb7b05
Tomomi Kimura <tomotomy73@gmail.com>
2022-12-20 11:24:47 +0900

    feat: style icon size, clean codes, and add bootstrap module css

--- packages/ui/shared/IconTile/IconTile.tsx
+++ packages/ui/shared/IconTile/IconTile.tsx
@@ -17,15 +17,6 @@ export type IconTileProps = {
   onClick?: () => void;
 };
 
-// DONE: Add Story to test icon, commit and push
-// FIXME: Style icon size properly
-// DONE: Create a issue/ticket for iconComponent (Github - e.g. #8)
-// DONE: Create Icon component (branch should be from main: feature/8-iconComponent)
-// DONE: Push commits and make PR in Github for review
-// DONE: Switch to feature/4-iconTile and merge feature/8-iconComponent into it (so that Icon component is available here)
-// DONE: Make the changes to use <Icon />, commit and push
-// TODO: Create a PR for feature/4-iconTile
-// TODO: Add icon to other components
 export const IconTile: React.FC<IconTileProps> = ({ title, description, onClick, image }) => {
   let imageContent: ReactNode = undefined;
   if (typeof image === "string") {
