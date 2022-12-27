d66ab50502ca0235be358d8d0d6629b342a1f6b6
Miguel Aragon <miguel@aragon.codes>
2022-12-27 20:32:45 +0900

    fix: move reset.css to global-imports

--- packages/ui/shared/global-imports.tsx
+++ packages/ui/shared/global-imports.tsx
@@ -1,5 +1,6 @@
 // This file aggregates all the imports that should be present in all applications
 // If something is imported here, it will be accessible everywhere
 
+import "ui/shared/reset.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "ui/shared/variables.css";
