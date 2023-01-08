3aea7906b2e05f71fe2c402480d42dc34241686b
Miguel Aragon <8313806+MiguelAraCo@users.noreply.github.com>
2023-01-08 14:02:01 +0900

    chore: rename docs app to admin (#22)
    Fixes #21

--- apps/admin/pages/_app.tsx
+++ apps/admin/pages/_app.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { AppProps } from "next/app";
 
-import "ui/shared/reset";
 import "ui/shared/global-imports";
 
 export const AdminApp: React.FC<AppProps> = ({ Component, pageProps }) => {
